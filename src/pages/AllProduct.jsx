469f0af09788097fc5cf3dfea5427061efbf9bbc
ShoaibCoder <123197246+ShoaibCoder@users.noreply.github.com>
2024-04-21 20:53:00 +0530

    Update AllProduct.jsx

--- src/pages/AllProduct.jsx
+++ src/pages/AllProduct.jsx
@@ -50,7 +50,7 @@ const AllProduct = () => {
                                     <div className="h-full border border-gray-300 rounded-xl overflow-hidden shadow-md cursor-pointer">
                                         <img
                                             onClick={() => navigate(`/productinfo/${id}`)}
-                                            className="lg:h-80  h-96 w-full"
+                                            className="lg:h-80  h-96 w-full hover:scale-90 transition-transform duration-300 ease-in-out"
                                             src={productImageUrl}
                                             alt="blog"
                                         />
