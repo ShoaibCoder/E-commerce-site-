2b4c8bcc112e9bf7eed00dee774620053e7b6cb0
ShoaibCoder <123197246+ShoaibCoder@users.noreply.github.com>
2024-04-21 20:35:00 +0530

    Update Category.jsx

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -2,27 +2,27 @@ import { useNavigate } from "react-router";
 
 const category = [
     {
-        image: 't_shirt.png',
+        image: 'https://image.uniqlo.com/UQ/ST3/eu/imagesother/2024/appeal/m-tshirts-mb.jpg',
         name: 't-shirt'
     },
     {
-        image: 'jacket.png',
+        image: 'https://getketchadmin.getketch.com/product/8905980505632/660/HLSS000869_1.JPG',
         name: 'jacket'
     },
     {
-        image: 'mobile.png',
+        image: 'https://images.unsplash.com/photo-1604409273943-ed5b1a083a51?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8',
         name: 'mobile'
     },
     {
-        image: 'laptop.png',
+        image: 'https://images.unsplash.com/photo-1595087873528-1fe582a3b302?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDF8fHxlbnwwfHx8fHw%3D',
         name: 'laptop'
     },
     {
-        image: 'https://p7.hiclipart.com/preview/624/596/891/nike-free-nike-air-max-sneakers-shoe-red-shoes.jpg',
+        image: 'https://images.unsplash.com/photo-1636718282214-0b4162a154f0?q=80&w=1530&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
         name: 'shoes'
     },
     {
-        image: 'sofa-2.png',
+        image: 'https://wakefitdev.gumlet.io/img/npl_modified_images/Magnus/WSFAMGNN3FNTN/1.jpg?w=732',
         name: 'home'
     }
     
@@ -39,9 +39,9 @@ const Category = () => {
                         {category.map((item, index) => {
                             return (
                                 <div key={index} className="px-3 lg:px-10">
-                                    <div onClick={() => navigate(`/category/${item.name}`)} className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs border-2 border-gray-500 rounded-3xl 12px bg-gradient-to-r from-[#e5dce0] to-[#ce9a09]  transition-all  hover:bg-white cursor-pointer mb-1 "  >
+                                    <div onClick={() => navigate(`/category/${item.name}`)} className=" w-16 h-16 lg:w-24 lg:h-24 max-w-xs border-2 border-gray-500 rounded-3xl 12px transition-all hover:bg-white cursor-pointer mb-1 overflow-hidden cursor-pointer"  >
                                         <div className="flex justify-center mb-12 ">
-                                            <img src={item.image} alt="img" className="w-24 h-24" style={{ width: '100%', height: '100%' }} />
+                                            <img src={item.image} alt="img" className="w-24 h-24 rounded-md 12px hover:scale-110 transition-transform duration-300 ease-in-out" style={{ width: '100%', height: '100%' }} />
                                         </div>
                                       
                                     </div>
